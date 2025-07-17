020f7f11c9e8a7e49010ce7d8d8029483719b8ab
harshrajurkar <harshrajurkar201@gmail.com>
2025-07-17 16:05:59 +0530

    Keep only Item ID field

--- app.py
+++ app.py
@@ -42,5 +42,16 @@ def view():
 
     data = {"data":data}
     return data
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    item_name = request.form.get('itemName')
+    item_desc = request.form.get('itemDescription')
+    
+    collection.insert_one({
+        "name": item_name,
+        "description": item_desc
+    })
+    
+    return "To-Do item submitted successfully!"
 if __name__ == "__main__":
     app.run(debug=True)
